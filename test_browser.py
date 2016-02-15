7f42ebc5da8684fc8ad59b3b692ba38ea038ab90
Jim Owens <jim@26maidenlane.net>
2016-02-15 15:21:09 -0500

    Update test_browser.py

--- test_browser.py
+++ test_browser.py
@@ -3,9 +3,8 @@ from browser import Browser
 from argparse import ArgumentParser
 
 def main(num_agents, urls=None):
-    # The list of URLs for agents to visit. These are for
-    # Jim's home network and must therefore be replaced
-    # to run on the range.
+    # The list of URLs for agents to visit. These are for Jim's home network 
+    # and must therefore be replaced to run on the range.
     if urls == None:
         urls = ['betaport.26maidenlane.net', 'betabank.26maidenlane.net', 'tncc.26maidenlane.net', 'pots.26maidenlane.net', 'gh.26maidenlane.net', 'pha.26maidenlane.net', 'dantes.26maidenlane.net', 'cfa.26maidenlane.net','wbpr.26maidenlane.net']
     # A list of browsing-agent threads
